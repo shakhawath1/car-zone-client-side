307eb5a730766f9a942bfec5b1fd103685e10899
shakhawath <m.shakhawath18@gmail.com>
2022-05-09 01:06:22 +0600

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 // import './App.css';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
 
     </div>
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div id="error-page">
+            <div className="content">
+                <h2 className="header" data-text="404">
+                    404
+                </h2>
+                <h4 data-text="Opps! Page not found">
+                    Opps! Page not found
+                </h4>
+                <p>
+                    Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem.
+                </p>
+                <div className="btns">
+                    <Nav.Link as={Link} to="/">Return Home</Nav.Link>
+                    {/* <a href="https://www.codingnepalweb.com/">report problem</a> */}
+                </div>
+            </div>
         </div>
     );
 };
