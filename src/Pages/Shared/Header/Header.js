8fd8ad2fbac48dc3ce3f03583655df4fc1c28a99
shakhawath <m.shakhawath18@gmail.com>
2022-05-09 13:15:49 +0600

    added private route for user login

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png';
 
 const Header = () => {
+
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
@@ -19,10 +27,21 @@ const Header = () => {
                         <Nav.Link as={Link} to="about">About me</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={Link} to="manage-item">Manage Items</Nav.Link>
-                        <Nav.Link as={Link} to="add-car">Add Car</Nav.Link>
-                        <Nav.Link as={Link} to="about">My Car</Nav.Link>
-                        <Nav.Link as={Link} to="login">Login</Nav.Link>
+                        {
+                            user && <>
+                                <Nav.Link as={Link} to="manage-item">Manage Items</Nav.Link>
+                                <Nav.Link as={Link} to="add-car">Add Car</Nav.Link>
+                                <Nav.Link as={Link} to="about">My Car</Nav.Link>
+                            </>
+
+                        }
+                        {
+                            user ?
+                                <button className='btn btn-link text-dark text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                :
+                                <Nav.Link as={Link} to="login">
+                                    Login
+                                </Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
